1be9a2f6196fe7d052ef24522170ba29d9581d45
Adenegan Oluwafeyisayomi <ooluwafeyisayomi@gmail.com>
2021-07-23 19:44:16 +0200

    added get

--- stock_prediction/app.py
+++ stock_prediction/app.py
@@ -20,7 +20,7 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['POST', 'GET'])
 def predict():
     period = int(request.form["horizon"])
     stock = request.form["stock"]
